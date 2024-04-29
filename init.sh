17ae3c20376c59a4d29d8953f7c63e2614161c07
Denys Chernousov <blackInOwls@gmail.com>
2024-04-29 23:49:10 +0300

    set for inner

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=("next-reviews.darkinowls.com" "promocodes-trofim.darkinowls.com")
+domains=(next-reviews.darkinowls.com promocodes-trofim.darkinowls.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="blackinowls@gmail.com" # Adding a valid address is strongly recommended
